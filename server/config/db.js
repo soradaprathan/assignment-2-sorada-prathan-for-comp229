68c6c01be94133bb3a811c5f65a571f03f633cff
Sorada Prathan <sprathan@my.centennialcollege.ca>
2023-02-14 11:24:46 -0500

    updateddb URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/contact_list"
+    // "URI": "mongodb://127.0.0.1/contact_list"
+    "URI": "mongodb+srv://sorada:PvqGgaXZygfWmxt3@mongodbserver.lxjwg9r.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
 
 // mongodb://127.0.0.1
